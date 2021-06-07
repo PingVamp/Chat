fefffeaa0ced2eb621873978810eed8ae4db8bf8
Vladimir Kortushov <vladx2000@mail.ru>
2021-06-07 14:52:38 +0300

    Class User added, 2 files - User.h, User.cpp

---
+++ Chat/Chat/User.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "User.h"
+
+
+User::User(string username, string password, Message& ptrCurrentMessage)
+{
+    _username = username;
+    _password = password;
+    this->_ptrCurrentMessage = ptrCurrentMessage;
+}
+
+User::User(string username) 
+{
+    _username = username;
+}
+
+User::User()
+{
+}
+
+User::~User()
+{
+}
+
+string User::getUserName() const
+{
+    return string(_username);
+}
+
+Message& User::getCurrentMessage() const
+{
+    return this->_ptrCurrentMessage;
+}
+
+void User::setUserName(string username)
+{
+    _username = username;
+}
+
+void User::DisplayUserName() const
+{
+    cout << this->_username << endl;
+}
+
+void User::setPassword(string password)
+{
+    _password = password;
+}
+
+void User::setCurrentMessage(Message& ptrCurrentMessage)
+{
+    this->_ptrCurrentMessage = &ptrCurrentMessage;
+}
--- Chat/Chat/User.h
+++ Chat/Chat/User.h
@@ -1,5 +1,29 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
+#include "Message"
+#include <string>
 
+using namespace std;
+
+class User
+{
+private:
+    string _username; // имя пользователя
+    string _password; // пароль
+    Message* _ptrCurrentMessage = 0; // указатель на текущее сообщение
+
+public:
+    User(string username, string password, Message& ptrCurrentMessage);
+    User(string username);
+    User();
+
+    ~User();
+
+    string getUserName() const;
+    Message& getCurrentMessage() const;
+    void DisplayUserName() const;
+    void setUserName(string username);
+    void setPassword(string password);
+    void setCurrentMessage(Message& ptrCurrentMessage);
+};
