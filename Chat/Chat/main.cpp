a35869c29740be3a6c7a8b40ecef529086d95e16
Gleb Skutin <glebskutin@gmail.com>
2021-06-07 20:35:11 +0500

    Прописал функцию регистрации новых пользователей. Пока лежит в main.cpp

--- Chat/Chat/main.cpp
+++ Chat/Chat/main.cpp
@@ -2,19 +2,95 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include "Message.h"
+#include "User.h"
+#include <Windows.h>
+using namespace std;
+
+
+vector<Message> messages;
+vector<User> users = { User("Вася", "123"), User{"Петя","321"} };
+void registration()
+{
+	bool regpass = 0;
+	string regname;
+	string regpassword;
+	while (regpass == 0)
+	{
+		cout << "Регистрация" << endl;
+		cout << "Введите желаемое имя пользователя" << endl;
+		cin >> regname;
+		for (int i = 0; i < users.size(); i++)
+		{
+			if (regname == users[i].getUserName())
+			{
+				cout << "Имя занято. Введите другое имя" << endl;
+				break;
+			}
+			else if (i + 1 == users.size())
+			{
+				cout << "Введите пароль" << endl;
+				cin >> regpassword;
+				if (regpassword == "")
+				{
+					cout << "Пароль должен содержать как минимум 1 символ" << endl;
+				}
+				else
+				{
+					users.push_back(User(regname, regpassword));
+					cout << "Регистрация успешна" << endl;
+					regpass = 1;
+					break;
+				}
+			}
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	
+	bool exit = 0;
+
+	string command = "";
+
+
+	while (exit != 1)
+	{
+		while (command != "exit")
+		{
+			cout << "Введите enter для входа или register для регистрации" << endl;
+			cout << "Для выхода введите exit" << endl;
+			cin >> command;
+			if (command == "exit")
+			{
+				exit = 1;
+				continue;
+			}
+			if (command == "register")
+			{
+				registration();
+			}
+			for (int i = 0; i < users.size(); i++)
+			{
+				users[i].DisplayUserName();
+			}
+		}
+	}
+
+	/*messages.push_back(Message("Привет", "Вася"));
+	messages.push_back(Message("Хэллоу", "Петя"));
+	messages.push_back(Message("Хэллоу", "Петя", "Вася"));
+	for (int i = 0; i < messages.size(); i++)
+	{
+		messages[i].Showmessage();
+	}*/
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
